d3960b63e13c735b4a2fb956fee51b6ad511b978
Andrew Schmelyun <schmelyun@gmail.com>
2020-08-26 03:16:16 -0400

    Adds src directory, checks for single README.md file to delete before git clone

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,11 @@ ls -la
 
 if [[ ! -f /var/www/html/cleaver ]]
 then
+    if [[ -f /var/www/html/README.md ]]
+    then
+        rm /var/www/html/README.md
+    fi
+
     git clone https://github.com/aschmelyun/cleaver.git /var/www/html && rm -r .git
 fi
 
